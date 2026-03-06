08a75e08dc627b2a7936be8096bf7608acedfbbe
anupam5671 <anupam4@ualberta.ca>
2026-03-06 16:35:53 -0700

    Add failing test for countCities

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -16,6 +16,18 @@ public class CustomList {
     public void deleteCity(City city) {
         cities.remove(city);
     }
+    @Test
+    public void testCountCities() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        City edmonton = new City("Edmonton", "AB");
+
+        list.addCity(calgary);
+        list.addCity(edmonton);
+
+        // This will fail initially because countCities() doesn't exist
+        assertEquals(2, list.countCities());
+    }
 
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
